3fd9af733285230f424760bc20538a5c0c776d5c
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-05-03 13:53:07 +0100

    add comments, change table schema so we can join on stack name/id

--- resources/table.go
+++ resources/table.go
@@ -10,24 +10,34 @@ import (
 	"github.com/guardian/cq-source-cloudformation-template-summary/client"
 )
 
+type TemplateSummary struct {
+	Metadata  string
+	StackName string
+	StackId   string
+}
+
 func SampleTable() *schema.Table {
 	return &schema.Table{
 		Name:      "cloudformation_template_summary_sample_table",
 		Resolver:  fetchTemplateSummaries,
-		Transform: transformers.TransformWithStruct(&cloudformation.GetTemplateSummaryOutput{}),
+		Transform: transformers.TransformWithStruct(&TemplateSummary{}),
 	}
 }
 
-// returns template summaries for all stacks in the account
+// fetchTemplateSummaries fetches a list of template summaries from the CloudFormation API
 func fetchTemplateSummaries(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+	// Create a new client using the meta object passed in
 	c := meta.(*client.Client)
-	stacks, err := c.CfnClient.ListStacks(ctx, &cloudformation.ListStacksInput{})
 
+	// Call the ListStacks API to get a list of stacks
+	stacks, err := c.CfnClient.ListStacks(ctx, &cloudformation.ListStacksInput{})
 	if err != nil {
 		return err
 	}
 
+	// Iterate through each stack in the list of stacks
 	for _, stack := range stacks.StackSummaries {
+		// Call the GetTemplateSummary API to get the template summary for the stack
 		stackName := stack.StackName
 		input := &cloudformation.GetTemplateSummaryInput{StackName: stackName}
 		summary, err := c.CfnClient.GetTemplateSummary(ctx, input)
@@ -37,7 +47,14 @@ func fetchTemplateSummaries(ctx context.Context, meta schema.ClientMeta, parent 
 			}
 			return err
 		}
-		res <- summary
+
+		// Create a new TemplateSummary object and populate it with the information from the API call
+		table := &TemplateSummary{
+			Metadata:  *summary.Metadata,
+			StackName: *stack.StackName,
+			StackId:   *stack.StackId,
+		}
+		res <- table
 	}
 	return nil
 }
